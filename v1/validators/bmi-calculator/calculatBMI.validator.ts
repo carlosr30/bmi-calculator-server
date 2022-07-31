78903779ecfb5330ccbaf67cb785b4dd2133d61c
Carlos Resultay <resultaycarlos@gmail.com>
2022-07-31 20:36:41 +0800

    Modify validator

--- v1/validators/bmi-calculator/calculatBMI.validator.ts
+++ v1/validators/bmi-calculator/calculatBMI.validator.ts
@@ -14,12 +14,24 @@ export const calculateBMIValidator = async (
             measurementUnit: Joi.string()
                 .allow(...bmiConfig.classifications)
                 .required(),
-            height: Joi.number().greater(0).required(),
-            heightSecondary: Joi.number().greater(0).when("measurementUnit", {
-                is: "FT_IN_AND_LBS",
-                then: Joi.required(),
-                otherwise: Joi.disallow(),
+            height: Joi.when("measurementUnit", {
+                is: "M_AND_KG",
+                then: Joi.number().greater(0).required(),
+                otherwise: Joi.when("heightSecondary", {
+                    is: "0",
+                    then: Joi.number().integer().min(1).required(),
+                    otherwise: Joi.number().integer().min(0).required(),
+                }),
             }),
+            heightSecondary: Joi.number()
+                .integer()
+                .min(0)
+                .max(11)
+                .when("measurementUnit", {
+                    is: "FT_IN_AND_LBS",
+                    then: Joi.required(),
+                    otherwise: Joi.disallow(),
+                }),
             weight: Joi.number().greater(0).required(),
         }).validate(req.body, { abortEarly: false })
 
